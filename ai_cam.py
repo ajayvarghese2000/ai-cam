87c9b65f186c049f5dfeff0a987a2e4e43215c89
ajayvarghese2000 <58085441+ajayvarghese2000@users.noreply.github.com>
2022-03-02 17:21:10 +0000

    Serial too slow, disabled old method

--- ai_cam.py
+++ ai_cam.py
@@ -7,15 +7,17 @@ class ai_cam:
         # Creating the camera object with the supplied variables
         self._CAM = camera(CAMID, WIDTH, HEIGHT, FPS, WEIGHTS, CFG, COCO)
 
-        self._serialport = serial.Serial("/dev/ttyS0", 9600, timeout=1)
+        self._serialport = serial.Serial("/dev/ttyS0", 115200, timeout=1)
         self._serialport.flush()
         return
+
+    ## Too slow
     def get_frame_serial(self):
         frame, person = self._CAM.getFrame()
         self._serialport.write(str.encode(frame + '\n'))
         return
 
-
+'''
 CAMID = 0 			                                # The Cam ID, usually 0, but if you have many cams attached it may change
 CAM_HEIGHT = 360	                                # The height of the camera frame, higher you go, slower performance (don't change unless needed)
 CAM_WIDTH = 640		                                # The width of the camera frame, higher you go, slower performance (don't change unless needed)
@@ -26,5 +28,5 @@ COCO= "detection models/coco.names"                 # Files path to the COCO nam
 
 cam = ai_cam(CAMID, CAM_WIDTH, CAM_HEIGHT, FPS, WEIGHTS, CFG, COCO)
 
-while 1:
-    cam.get_frame_serial()
+cam.get_frame_serial()
+'''
